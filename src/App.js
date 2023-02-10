4b5263eeb97c9b21f4cfe95acd1bf2b1d2bbaebd
Mohit Kushwaha <mohit@uniftec.com>
2023-02-10 12:42:09 +0530

    added the code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import './App.css';
 import Header from './components/headers/Header';
 import Pages from './components/mainPages/Pages';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route,Navigate   } from 'react-router-dom'
 import ContractoForm from './components/mainPages/contractorForm/ContractoForm'
 function App() {
   return (
     <div className="App">
      <Header/>
-      <Routes>
-        <Route path='/' element = {<> <Pages/> </>} />
-         <Route path="contractor-form" element={<div  className="container"> <ContractoForm/></div>  } />
-         <Route path="vendor-form" element={<ContractoForm/>  } />
-        </Routes>
+      <ContractoForm/>
     </div>
   );
 }
