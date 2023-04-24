f9fd64334b62b1f3af48ff6cb952dcd4eabafc38
Mohit Kushwaha <mohit@uniftec.com>
2023-04-24 12:51:45 +0530

    added hte code

--- src/services/Vendor.js
+++ src/services/Vendor.js
@@ -58,3 +58,16 @@ export const search_vendor = (fromData)=>{
         })
     
 }
+
+
+export const remove_vendor = (fromData)=>{
+    return ()=>
+        new Promise((resolve,reject)=>{
+            Http.delete(vendor_url+'/remove_vendor/'+fromData).then((res)=>{
+                return resolve(res.data)
+            }).catch((err)=>{
+                return reject(err);
+            })
+        })
+    
+}
